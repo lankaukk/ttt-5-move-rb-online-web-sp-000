8a503b5e9089b3ba6b3795ba2843b335b08c5a12
McKayla Lankau <lankaukk@yahoo.com>
2020-07-13 03:23:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,8 +9,6 @@ def move(board, index, char = "X")
     board[index] = char
 end
 
-board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
