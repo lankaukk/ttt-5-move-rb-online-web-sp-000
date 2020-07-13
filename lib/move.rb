3aa3eae1601f5f0d88f55c91039f5ff32ecdaccf
McKayla Lankau <lankaukk@yahoo.com>
2020-07-13 03:23:10 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ end
 
 board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
 
-def display_board ( board )
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
